e06970af51146016b5d94ceddabc3e510d52f3f5
vsouvatz <vassilis.souvatzis@intrasoft-intl.com>
2022-01-27 20:14:46 +0200

    project setup

---
+++ src/main/java/vs/elasticbloodmeasurements/ElasticBloodMeasurementsApplication.java
@@ -0,0 +1,13 @@
+package vs.elasticbloodmeasurements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBloodMeasurementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBloodMeasurementsApplication.class, args);
+	}
+
+}
